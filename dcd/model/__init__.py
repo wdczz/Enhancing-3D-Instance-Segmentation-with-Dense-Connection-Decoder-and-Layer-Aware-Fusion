b2ed7533f0636ce02730f183f74e3ad0a85d57f4
wdczz <walfwalf@163.com>
2025-08-19 16:57:08 +0800

    Initial commit: 3D Instance Segmentation with Dense Connection Decoder

---
+++ dcd/model/__init__.py
@@ -0,0 +1,1 @@
+from .dcd import DCD
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name='dcd',
+        version='1.0',
+        description="Enhancing 3D Instance Segmentation with Dense Connection Decoder and Layer Aware Fusion (RAL2025)",
+        author='Duanchu Wang',
+        author_email='wangduanchu@stu.xidian.edu.cn',
+        packages=['dcd'],
+    )
